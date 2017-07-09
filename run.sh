cfa669ef2d7976b106ae9c340929400502e5f75e
Josiah Dahl <josiah.dahl@gmail.com>
2017-07-09 10:16:49 -0700

    Fix issue with network needing to be created - start reverse proxy first

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting Reverse Proxy Demo"
+(cd reverse-proxy && docker-compose up -d)
 (cd site1 && docker-compose up -d)
 (cd site2 && docker-compose up -d)
 (cd site3 && docker-compose up -d)
-(cd reverse-proxy && docker-compose up)
